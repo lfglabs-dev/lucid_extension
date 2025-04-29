ea006f6437d196c24a12db260ad99425fd0c65b0
Iris <iris.devillars@gmail.com>
2025-04-29 17:08:45 +0400

    feat: permit requests (#9)
    * feat: add events to send request
    
    * feat: add permit type request

--- src/types.ts
+++ src/types.ts
@@ -69,43 +69,56 @@ export const SIGNING_METHODS = [
 /**
  * List of transaction methods that are currently supported for monitoring
  */
-export const SUPPORTED_METHODS: string[] = [
-  'eth_sendTransaction',
-  'eth_signTypedData_v4',
-] as const;
+export const SUPPORTED_METHODS: string[] = ['eth_sendTransaction', 'eth_signTypedData_v4'] as const;
 
 export type SigningMethod = (typeof SIGNING_METHODS)[number];
 
-  /**
-   * Type for EIP-712 transaction data
-   */
-  export interface EIP712SafeTx {
-    chainId: string;
-    safeAddress: string;
-    from: string;
-    to: string;
-    value: string;
-    data: string;
-    operation: string;
-    safeTxGas: string;
-    baseGas: string;
-    gasPrice: string;
-    gasToken: string;
-    refundReceiver: string;
-    nonce: string;
-  }
+/**
+ * Type for EIP-712 transaction data
+ */
+export interface EIP712SafeTx {
+  chainId: string;
+  safeAddress: string;
+  from: string;
+  to: string;
+  value: string;
+  data: string;
+  operation: string;
+  safeTxGas: string;
+  baseGas: string;
+  gasPrice: string;
+  gasToken: string;
+  refundReceiver: string;
+  nonce: string;
+}
 
-  /**
-   * Type for regular EOA transactions
-   */
-  export interface EoaTx {
-    nonce: string;
-    chainId: string;
-    from: string;
-    to: string;
-    value: string;
-    data: string;
-    gas: string;
-    maxFeePerGas: string;
-    maxPriorityFeePerGas: string;
-  }
+/**
+ * Type for regular EOA transactions
+ */
+export interface EoaTx {
+  nonce: string;
+  chainId: string;
+  from: string;
+  to: string;
+  value: string;
+  data: string;
+  gas: string;
+  maxFeePerGas: string;
+  maxPriorityFeePerGas: string;
+}
+
+/**
+ * Type for regular Permit approval
+ */
+export interface PermitTx {
+  chainId: string;
+  coinName: string;
+  verifyingContract: string;
+  version: string;
+  from: string;
+  deadline: string;
+  nonce: string;
+  owner: string;
+  spender: string;
+  value: string;
+}
