4f126e11061b44d1a049995319fda0cbb63383b9
Thomas Marchand <thomas.marchand@tuta.io>
2025-05-01 11:38:04 +0100

    feat: compress data in an optimized qr code

--- src/popup.ts
+++ src/popup.ts
@@ -7,28 +7,24 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   try {
     const response = await getLinkToken();
-    const linkToken = response.data; // Extract token from response
+    const linkToken = response.data;
     console.log('Link token received:', linkToken);
-
-    const deepLink = `${APP_LINK}/--/connect?token=${linkToken}`;
-    const tokenOnlyUrl = `token-${linkToken}`;
     const deviceName = navigator.userAgent;
     const decryptionKey = await getEncryptionKey();
+    if (!decryptionKey) throw new Error('Failed to get encryption key');
 
-    const combinedData = JSON.stringify({
-      app: deepLink,
-      token: tokenOnlyUrl,
-      name: deviceName,
-      decryptionKey: decryptionKey,
-    });
+    // we encode everything in URL to avoid duplicates and allow our app to retrieve everything
+    const deepLink = `${APP_LINK}/--/connect?t=${linkToken}&n=${deviceName}&d=${decryptionKey.k}`;
 
-    const qrCodeDataUrl = await QRCode.toDataURL(combinedData, {
+    const qrCodeDataUrl = await QRCode.toDataURL(deepLink, {
       width: 200,
       margin: 2,
       color: {
         dark: '#000000',
         light: '#ffffff',
       },
+      // in this context it's better to have a readable qrcode than a recoverable code
+      errorCorrectionLevel: 'L',
     });
     console.log('QR code generated successfully');
 
